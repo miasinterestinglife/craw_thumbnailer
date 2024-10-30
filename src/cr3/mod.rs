867de6f5d91c60b0cd90adf562bf7b5458558790
Mia Kaanen <72391367+miasinterestinglife@users.noreply.github.com>
2024-10-30 12:13:29 +0100

    add orientation detection to cr3

--- src/cr3/mod.rs
+++ src/cr3/mod.rs
@@ -1,7 +1,6 @@
-use crate::read_file;
+use crate::{read_file, get_file_header};
 use crate::bytes_to_u32;
 use image::load_from_memory_with_format;
-
 
 pub fn extract_thumb(file_path: &String, output: &String, size: u16){
     let raw_file = read_file(&file_path);
@@ -9,6 +8,31 @@ pub fn extract_thumb(file_path: &String, output: &String, size: u16){
     let jpeg_size = bytes_to_u32(&raw_file[index+20..=index+23], &[0x4d,0x4d]);
     let raw_img = &raw_file[index+24..=index+24+jpeg_size as usize];
     let mut img = load_from_memory_with_format(raw_img, image::ImageFormat::Jpeg).unwrap();
+
+    //get Metadata to show correct Orientation
+    let exif_index = raw_file.windows(4).position(|w| w==[0x43, 0x4D, 0x54, 0x31]).unwrap()+4;
+    let exif_end_idx = raw_file.windows(4).position(|w| w==[0x43, 0x4D, 0x54, 0x32]).unwrap()-1;
+    let raw_exif = &raw_file[exif_index..=exif_end_idx];
+    let exif_internal_data = get_file_header(&raw_exif.to_vec());
+
+    for n in 0..exif_internal_data.ifds.as_ref().unwrap()[0].as_ref().unwrap().num_entries as usize{
+        let entry = &exif_internal_data.ifds.as_ref().unwrap()[0].as_ref().unwrap().entries.as_ref().unwrap()[n];
+        if entry.tag_id == 274{
+            match entry.pointer{
+                /*
+                1: rotate 0 degrees
+                6: rotate 90 degrees
+                3: rotate 180 degrees
+                8: rotate 270 degrees 
+                */
+                6 => img = img.rotate90(),
+                3 => img = img.rotate180(),
+                8 => img = img.rotate270(),
+                _ => {}
+            }
+        }
+    }
+
     let size_factor:f32;
     if size != 0{
         size_factor = size as f32 / img.width() as f32;
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,73 @@ struct Args {
     size:u16
 }
 
+#[derive(Debug)]
+struct InternalMeta{
+    byte_order: [u8;2],
+    tiff_ofs: u32,
+    cr2_ver: [u8;2],
+    raw_ifd_ofs: u32,
+    ifds: Option<[Option<IFDData>;4]>
+}
+
+#[derive(Debug)]
+struct IFDData{
+    num_entries: u16,
+    ofs: u32,
+    entries: Option<Vec<IFDEntry>>,
+    next_ifd_ofs: Option<u32>
+}
+
+#[derive(Debug)]
+struct IFDEntry{
+    tag_id: u16,
+    pointer: u32
+}
+
+fn read_ifd(raw_data: &Vec<u8>, offset:&u32, internal_data: &InternalMeta) -> IFDData{
+    let mut data = IFDData{
+        num_entries: 0,
+        ofs: *offset,
+        entries: Some(vec![]),
+        next_ifd_ofs: None
+    };
+    data.num_entries = bytes_to_u16(&raw_data[*offset as usize..=(*offset+1) as usize], &internal_data.byte_order);
+    let mut ifd_entries: Vec<IFDEntry> = vec![];
+    let last_ofs:usize = (data.ofs + 2+12*data.num_entries as u32) as usize;
+    for n in 0..data.num_entries as usize{
+        let ifd_ofs:usize;
+        if n==0{
+            ifd_ofs = data.ofs as usize + 2;
+        }
+        else {
+            ifd_ofs = data.ofs as usize +  2+12*(n);
+        }
+        let tag_id = bytes_to_u16(&raw_data[ifd_ofs..=ifd_ofs+1], &internal_data.byte_order);
+        let tag_pointer = bytes_to_u32(&raw_data[ifd_ofs+8..=ifd_ofs+11], &internal_data.byte_order);
+        ifd_entries.push(IFDEntry {tag_id: tag_id, pointer: tag_pointer })
+    }
+    data.entries.as_mut().unwrap().append(&mut ifd_entries);
+    data.next_ifd_ofs = Some(bytes_to_u32(&raw_data[last_ofs..=last_ofs+3], &internal_data.byte_order));
+    data
+}
+
+fn get_file_header(raw_data: &Vec<u8>)->InternalMeta{
+    let mut internal_data = InternalMeta{
+        byte_order: [0,0],
+        tiff_ofs: 0,
+        cr2_ver: [0,0],
+        raw_ifd_ofs: 0,
+        ifds: None
+    };
+    internal_data.byte_order = [raw_data[0], raw_data[1]];
+    internal_data.tiff_ofs = bytes_to_u32(&raw_data[4..=7], &internal_data.byte_order);
+    internal_data.cr2_ver = [raw_data[0xa],raw_data[0xb]];
+    internal_data.raw_ifd_ofs = bytes_to_u32(&raw_data[0xc..=0xf], &internal_data.byte_order);
+    let ifd0 = read_ifd(&raw_data, &internal_data.tiff_ofs, &internal_data);
+    internal_data.ifds = Some([Some(ifd0),None,None,None]);
+    internal_data
+}
+
 fn read_file(file_path: &String) -> Vec<u8>{
     let data = fs::read(file_path).unwrap();
     data
